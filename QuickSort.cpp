f12a829a83c8402f5f12e960653b63947e11a8c7
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-10 19:49:24 +0530

    Create QuickSort.cpp

---
+++ QuickSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void quickSort(vector<int> &nums, int s, int e){
+    
+    if(s>=e){
+        return;
+    }
+    int p = e;
+    int border = s-1;
+    for(int i=s; i<=e; i++){
+        if(nums[i] < nums[p]){
+            border++;
+            swap(nums[border] , nums[i]);
+        }
+    }
+    p = border+1;
+    swap(nums[p] , nums[e]);
+    
+    //int p = partition(nums, s, e);
+    quickSort(nums , s , p-1);
+    quickSort(nums , p+1 , e);
+}
+
+int main()
+{
+    vector<int>nums = {6,3,5,1,0,5};
+    int n = nums.size();
+    int s=0;
+    int e = n-1;
+    quickSort(nums, s, e);
+    for(int i=0; i<n; i++){
+        cout<<nums[i]<<"  ";
+    }
+    
+    return 0;
+}
