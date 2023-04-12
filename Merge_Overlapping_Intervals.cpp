9e2952eb9ddecd3f3730ba7b1e4fa4f71d70bcd2
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-12 18:03:07 +0530

    Update Merge_Overlapping_Intervals.cpp

--- Merge_Overlapping_Intervals.cpp
+++ Merge_Overlapping_Intervals.cpp
@@ -5,7 +5,6 @@ Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
 */
 
 
-
 vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
         sort(intervals.begin() , intervals.end());
@@ -30,10 +29,42 @@ vector<vector<int>> merge(vector<vector<int>>& intervals) {
                 pair[0] = intervals[i][0];
                 pair[1] = intervals[i][1];
             }
-
-            if(i==(n-1)){
-                ans.push_back(pair);
-            }
         }
+        ans.push_back(pair);    //for last pair which is not inserted
+
         return ans;
 }
+
+
+
+
+/*
+OR
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin() , intervals.end());
+        vector<vector<int>> ans;
+        int n = intervals.size();
+
+        if(n==0  ||  n==1){
+            return intervals;
+        }
+        vector<int> pair = intervals[0];
+
+        for(auto it : intervals){
+            if(pair[1] >= it[0]){
+                pair[1] = max(pair[1] , it[1]);
+            }
+            else{         //if(pair[1] < it[0]){
+                ans.push_back(pair);
+                pair = it;
+            }
+        }
+        ans.push_back(pair);       //for last pair which is not inserted
+
+        return ans;
+}
+
+*/
+
