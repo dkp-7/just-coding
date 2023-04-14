961f2c0c48db4d11b50832728d7c6b663fded37d
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-14 19:53:22 +0530

    Create search_in_2d.cpp

---
+++ search_in_2d.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();     //row size
+        int n = matrix[0].size();   //column size
+        bool ans = false;
+        int s = 0, e = m-1;        //for row
+        while(s <= e){
+            int mr = (s+e)/2;         //mid row
+            if( (target >= matrix[mr][0])   &&  (target <= matrix[mr][n-1]) ){
+                
+                int l=0, h=n-1;         //for column
+                while(l <= h){
+                    int mc = (l+h)/2;       //mid column
+                    if(target == matrix[mr][mc]){
+                        ans = true;
+                        return ans;
+                    }
+                    else if(target < matrix[mr][mc]){
+                        h = mc-1;
+                    }
+                    else if(target > matrix[mr][mc]){
+                        l = mc+1;
+                    }
+                }
+                return ans;
+                
+            }
+            else if(target > matrix[mr][n-1]){
+                s = mr+1;
+            }
+            else if(target < matrix[mr][0]){
+                e = mr-1;
+            }
+        }
+        return ans;
+    }
+
+int main()
+{
+    vector<vector<int>> matrix ={ {1,3,5,7},{10,11,16,20},{23,30,34,60} };
+    cout<<"Enter target element to search in array : ";
+    int target;
+    cin>>target;
+    bool ans = searchMatrix(matrix, target);
+    cout<<ans;
+    
+    return 0;
+}
