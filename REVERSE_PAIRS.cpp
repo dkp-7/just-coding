aea62a98e6c18cb74f5289bfd05b1debdfea10e9
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-19 17:18:54 +0530

    Update REVERSE_PAIRS.cpp

--- REVERSE_PAIRS.cpp
+++ REVERSE_PAIRS.cpp
@@ -1,3 +1,66 @@
+// leetcode :  493. Reverse Pairs
+
+class Solution {
+public:
+    
+    int merge(vector<int>& nums , int s, int e, int mid){
+        
+        int total=0;
+        int j = mid+1;
+        for(int i=s; i<=mid; i++){
+            while(j<=e  &&  (nums[i] > 2LL*nums[j]) ){
+                j++;
+            }
+            total += j-(mid+1);
+        }
+        
+        // actual merging starts from here
+        vector<int> temp;
+        int i=s;
+        j=mid+1;
+        
+        while(i<=mid  &&  j<=e){
+            if(nums[i] <= nums[j]){
+                temp.push_back(nums[i++]);
+            }
+            else{
+                temp.push_back(nums[j++]);
+            }
+        }
+        while(i<=mid){
+            temp.push_back(nums[i++]);
+        }
+        while(j<=e){
+            temp.push_back(nums[j++]);
+        }
+        int k=0;
+        for(int i=s; i<=e; i++){
+            nums[i] = temp[k++];
+        }
+        return total;
+    }
+    
+    int mergeSort(vector<int>& nums , int s, int e){
+        if(s>=e){
+            return 0;
+        }
+        int mid = (s+e)/2;        
+        int cnt = mergeSort(nums, s, mid);
+        cnt += mergeSort(nums, mid+1, e);
+        cnt += merge(nums, s, e, mid);
+        return cnt;
+    }
+    
+    int reversePairs(vector<int>& nums) {
+        int s=0;
+        int e=nums.size()-1;
+        int ans = mergeSort(nums, s, e);
+        return ans;
+    }
+};
+
+/*
+
 #include <iostream>
 #include <vector>
 
@@ -71,3 +134,5 @@ int main()
     cout<<"no. of reverse pairs : "<<ans;
     return 0;
 }
+
+*/
