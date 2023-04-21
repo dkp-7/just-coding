e2aa2b7c3808444d3e770b10e1f40c6374fefb86
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-21 20:18:37 +0530

    Update consecutiveSequence.cpp

--- consecutiveSequence.cpp
+++ consecutiveSequence.cpp
@@ -1,5 +1,82 @@
 // Leetcode :      128. Longest Consecutive Sequence
 
+// OPTIMAL APPROACH   TC: O(N)   {OR O(3N}
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        int n = nums.size();
+        if(n == 0) return 0;
+        
+        unordered_set<int> hashset;      //set stores only unique elements
+        
+        for(int i=0; i<n; i++){
+            hashset.insert(nums[i]);
+        }
+        int maxi = INT_MIN;
+        
+        for(auto it : hashset){           //one have to iterate through set / map elements like this
+            
+            if(hashset.find(it-1)  == hashset.end()){      //this means it is first element in consecutive order
+                int k = it;
+                int cnt=1; 
+                
+                while(hashset.find(k+1) != hashset.end()){
+                    cnt++;
+                    k = k+1;
+                }
+                maxi = max(maxi , cnt);
+            }
+            
+        }
+        return maxi;
+    }
+};
+
+
+/*
+
+// OPTIMAL APPROACH   TC: O(N)          {OR O(3N}
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        int n = nums.size();
+        if(n == 0) return 0;
+        
+        unordered_set<int> hashset;      //set stores only unique elements
+        
+        for(int i=0; i<n; i++){
+            hashset.insert(nums[i]);
+        }
+        int maxi = 1;
+        
+        for(auto it : hashset){           //one have to iterate through set / map elements like this
+            
+            if(hashset.find(it-1)  == hashset.end()){      //this means it is first element in consecutive order
+                int k = it;
+                int cnt=1; 
+                
+                while(hashset.find(k+1) != hashset.end()){
+                    cnt++;
+                    k = k+1;
+                }
+                maxi = max(maxi , cnt);
+            }
+            
+        }
+        return maxi;
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+//tc : O(n log n)
 
 class Solution {
 public:
