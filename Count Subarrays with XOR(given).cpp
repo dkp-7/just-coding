d2ae670162597267774c747d55241de45bb17ed0
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-24 12:46:24 +0530

    Create Count Subarrays with XOR(given).cpp

---
+++ Count Subarrays with XOR(given).cpp
@@ -0,0 +1,30 @@
+//Optimised code
+
+
+
+
+
+
+/*
+
+#include <bits/stdc++.h>
+
+int subarraysXor(vector<int> &arr, int x)
+{
+    //    Write your code here.
+    int cnt = 0;
+    int n = arr.size();
+    for(int i=0; i<n; i++){
+        int k = 0;
+        for(int j=i; j<n; j++){
+            k = k^arr[j];
+            if(k == x){
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+    
+}
+
+*/
