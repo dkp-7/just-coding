dbb87584e163911547b709b4effaf0f8753f7818
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-24 12:47:12 +0530

    Update Count Subarrays with XOR(given).cpp

--- Count Subarrays with XOR(given).cpp
+++ Count Subarrays with XOR(given).cpp
@@ -1,7 +1,46 @@
 //Optimised code
 
 
+#include <iostream>
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int subarraysXor(vector<int> &arr, int x)
+{
+    //    Write your code here.
+    int n = arr.size();
+    unordered_map<int,int> um;
+    int cnt = 0;
+    int xr=0;
+    
+    for(int i=0; i<n; i++){
+        
+        xr = xr^arr[i];
+        
+        if(xr == x){
+            cnt++;
+        }
+        int y = xr^x;
+        if(um.find(y) != um.end()){
+            cnt = cnt + um[y];
+        }
+        um[xr]++;
+    }
+    return cnt;
+}
+
+
+int main()
+{
+    //cout<<"Hello World";
+    vector<int> arr{ 5, 6, 7, 8, 9};
+
+    int totalCount= subarraysXor(arr,5);
+    cout<<"The total number of subarrays having a given XOR k is "<<totalCount<<endl;
+
+    return 0;
+}
 
 
 
