f26220f8ea7703204d8116ff830038be2ea43dea
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-26 14:06:38 +0530

    Update Intersection of Two Linked Lists.cpp

--- Intersection of Two Linked Lists.cpp
+++ Intersection of Two Linked Lists.cpp
@@ -1,6 +1,41 @@
 // Leetcode :       160. Intersection of Two Linked Lists  
 
 
+//Optimal Solution using Hashing  (T.C : O(sizeof list1 + size of list2) )
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        unordered_set<ListNode*> set;
+        
+        while(headA != NULL){
+            set.insert(headA);
+            headA = headA->next;
+        }
+        while(headB != NULL){
+            if(set.find(headB) != set.end()){
+                return headB;
+            }
+            headB = headB->next;
+        }
+        return NULL;
+    }
+};
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //Brute force approach
 
 class Solution {
