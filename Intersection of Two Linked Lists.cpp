f4100d99258260e9abe48835420d23c65efbd1bc
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-26 15:46:29 +0530

    Update Intersection of Two Linked Lists.cpp

--- Intersection of Two Linked Lists.cpp
+++ Intersection of Two Linked Lists.cpp
@@ -1,7 +1,7 @@
 // Leetcode :       160. Intersection of Two Linked Lists  
 
 
-//Optimal Solution using Hashing  (T.C : O(sizeof list1 + size of list2) )
+// Solution using difference of length of given two list
 
 /**
  * Definition for singly-linked list.
@@ -11,6 +11,52 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        int c1=0, c2=0;
+        ListNode* headA1 = headA;
+        ListNode* headB1 = headB;
+        
+        while(headA1 != NULL){
+            c1++;
+            headA1 = headA1->next;
+        }
+        while(headB1 != NULL){
+            c2++;
+            headB1 = headB1->next;
+        }
+        int diff = abs(c1-c2);
+        int maxi = max(c1 , c2);
+        
+        if(maxi == c1){
+            for(int i=0; i<diff; i++){
+                headA = headA->next;
+            }
+        }
+        else{
+            for(int i=0; i<diff; i++){
+                headB = headB->next;
+            }
+        }
+        
+        while((headA != headB)  &&  headA != NULL  &&  headB != NULL){
+            headA = headA->next;
+            headB = headB->next;
+        }
+        if(headA == headB) return headA;
+        else return NULL;
+    }
+};
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Solution using Hashing  (T.C : O(sizeof list1 + size of list2) )
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
