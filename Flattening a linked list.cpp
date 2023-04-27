0d957cdcc0b939bf67aab9d5165c926b2a58200f
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-27 20:28:16 +0530

    Create Flattening a linked list.cpp

---
+++ Flattening a linked list.cpp
@@ -0,0 +1,65 @@
+// Question : Flattening a linked list (GFG : https://practice.geeksforgeeks.org/problems/flattening-a-linked-list/1 )
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// this below code didn't worked   (In this I was creating a new linked list to store the flattened linked list)
+
+Node *flatten(Node *root)
+{
+   // Your code here
+   Node* l1 = root;
+   Node* l2 = root->next;
+   
+   
+   Node* ans = new Node(0);
+   Node* ansHead = ans->bottom;
+   
+   while(l2 != NULL){
+       Node* l2_next = l2->next;
+       while(l1 != NULL  &&  l2 != NULL){
+           
+           Node* temp = new Node(0);
+           //Node* temp1 = temp->bottom;
+           
+           if(l1->data <= l2->data){
+               temp->data = l1->data;
+               l1 = l1->bottom;
+           }
+           else{
+               temp->data = l2->data;
+               l2 = l2->bottom;
+           }
+           
+           ans->bottom = temp;
+           ans = temp;
+       }
+       
+       while(l1 != NULL){
+           Node* temp = new Node(0);
+           //Node* temp1 = temp->bottom;
+           temp->data = l1->data;
+           ans->bottom = temp;
+           ans = temp;
+           l1 = l1->bottom;
+           //l2 = l2->bottom;
+       }
+       
+       while(l1 != NULL){
+           Node* temp = new Node(0);
+           //Node* temp1 = temp->bottom;
+           temp->data = l2->data;
+           ans->bottom = temp;
+           ans = temp;
+           //l1 = l1->bottom;
+           l2 = l2->bottom;
+       }
+       
+       l1 = ansHead;
+       l2 = l2_next;
+   }
+   return ansHead;
+}
