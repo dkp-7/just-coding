c19f59ab818075b7a5ccc8e4e80b1b5f1ed91655
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-27 17:41:43 +0530

    Update Linked List Cycle II.cpp

--- Linked List Cycle II.cpp
+++ Linked List Cycle II.cpp
@@ -23,8 +23,33 @@ public:
                 return fast;  //return slow;
             }
         }
-        // After 1st collision set the past pointer to head sgsin & move both fast & slow ptr now.. one step at a time. Their 2nd collision will be the starting node of the linked list loop (to know why : see Floyd's Cycle-Finding algorithm)
+        // After 1st collision set the past pointer to head sgsin & move both fast & slow ptr now.. one step at a time,
+        //their 2nd collision will be the starting node of the linked list loop (to know why : see Floyd's Cycle-Finding algorithm)
         
+        return NULL;
+    }
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Using Hashing
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        unordered_set<ListNode*> hashset;
+        while(head != NULL){
+            
+            if(hashset.find(head) != hashset.end()){
+                return head;
+            }
+            hashset.insert(head);
+            head = head->next;
+        }
         return NULL;
     }
 };
