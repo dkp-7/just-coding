6297c0b253cf530c291ea365d326bb6dbbb3623e
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-27 20:57:59 +0530

    Update Flattening a linked list.cpp

--- Flattening a linked list.cpp
+++ Flattening a linked list.cpp
@@ -1,6 +1,42 @@
 // Question : Flattening a linked list (GFG : https://practice.geeksforgeeks.org/problems/flattening-a-linked-list/1 )
 
 
+Node* merge(Node* a, Node* b){
+    
+    Node* temp = new Node(0);
+    Node* temp1 = temp;
+    while(a != NULL  &&  b != NULL){
+        
+        if(a->data < b->data){
+            temp->bottom = a;
+            temp = temp->bottom;
+            a = a->bottom;
+        }
+        else{
+            temp->bottom = b;
+            temp = temp->bottom;
+            b = b->bottom;
+        }
+        
+        if(a) temp->bottom = a;
+        else temp->bottom = b;
+    }
+    return temp1->bottom;
+}
+    
+    
+Node *flatten(Node *root)
+{
+   if(root->next == NULL){
+       return root;
+   }
+   
+   Node* b = flatten(root->next);
+   
+   Node* ans = merge(root , b);
+   
+   return ans;
+}
 
 
 
