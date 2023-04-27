94ac332a8d74af98e453327350da639e98dd871e
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-04-27 17:31:14 +0530

    Create Linked List Cycle II.cpp

---
+++ Linked List Cycle II.cpp
@@ -0,0 +1,30 @@
+// Leetcode :     142. Linked List Cycle II
+
+//Most optimal approach
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        if(head == NULL  ||  head->next == NULL) return NULL;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        while(fast->next != NULL  &&  fast->next->next !=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow){
+                fast = head;
+                while(fast != slow){
+                    fast= fast->next;
+                    slow = slow->next;
+                }
+                return fast;  //return slow;
+            }
+        }
+        // After 1st collision set the past pointer to head sgsin & move both fast & slow ptr now.. one step at a time. Their 2nd collision will be the starting node of the linked list loop (to know why : see Floyd's Cycle-Finding algorithm)
+        
+        return NULL;
+    }
+};
