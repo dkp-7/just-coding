06152fcede1bcc432919f9c583f3fddd2053b293
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-01 12:40:44 +0530

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,5 +1,40 @@
-// Leetcode :      15. 3Sum (distinct pairs)
+// Leetcode :      15. 3Sum (distinct pairs , here tArget is 0)
 
+
+// OPtimised Approach usin 2 pointer 
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        int n= nums.size();
+        sort(nums.begin() , nums.end());
+        for(int i=0; i<n; i++){
+            if(i>0  &&  nums[i] == nums[i-1]) continue;
+
+            int l = i+1;
+            int r = n-1;
+            while(l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == 0){
+                    vector<int> pair = {nums[i] , nums[l] , nums[r]};
+                    ans.push_back(pair);
+                    l++; r--;
+                    while(nums[l] == nums[l-1]  &&  l<r) l++;
+                    while(nums[r] == nums[r+1]  &&  l<r) r--;
+                }
+                if(sum < 0) l++;
+                else if(sum > 0) r--;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // BRUTE Force Approach   (TLE)
