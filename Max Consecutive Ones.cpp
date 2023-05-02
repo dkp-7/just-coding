006d42581ed9cf6870ced6cb7a77272218e65a3a
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-02 23:08:21 +0530

    Update Max Consecutive Ones.cpp

--- Max Consecutive Ones.cpp
+++ Max Consecutive Ones.cpp
@@ -1,6 +1,29 @@
 // Leetcode : 485. Max Consecutive Ones
 
 
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+        int cnt = 0; int ans=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 1){
+                cnt++;
+            }
+            else{
+                cnt=0;
+            }
+            ans = max(ans , cnt);
+        }
+        
+        return ans;
+    }
+};
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 class Solution {
 public:
