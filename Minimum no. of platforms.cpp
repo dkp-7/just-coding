c66cfb0d6eca0c5621b434c4cbd54a7634287f25
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-03 20:29:58 +0530

    Update Minimum no. of platforms.cpp

--- Minimum no. of platforms.cpp
+++ Minimum no. of platforms.cpp
@@ -1,5 +1,7 @@
 // Minimum no. of platforms
 //Link : Given arrival and departure times of all trains that reach a railway station. Find the minimum number of platforms required for the railway station so that no train is kept waiting.
+//Link : https://www.codingninjas.com/codestudio/problems/799400?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=1
+
 /*
 Consider that all the trains arrive on the same day and leave on the same day. Arrival and departure time can never be the same for a train but we can have arrival time of one train equal to departure time of the other. At any given instance of time, same platform can not be used for both departure of a train and arrival of another train. In such cases, we need different platforms.
 
@@ -69,7 +71,7 @@ int calculateMinPatforms(int at[], int dt[], int n) {
     int pf = 1;
     while(i<=n-1){
         
-        //last train is still standing at platform, for new train arriving an extra platform is needed
+        //last train is still standing at platform, for new arriving train an extra platform is needed
        if(at[i] <= dt[j]){
            i++;
            pf++;
