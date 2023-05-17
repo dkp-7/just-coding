ef010b22f38e134ecf8c759d36c6351183656465
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-17 23:27:12 +0530

    Update Subsets II.cpp

--- Subsets II.cpp
+++ Subsets II.cpp
@@ -1,6 +1,41 @@
 // Leetcode :    90. Subsets II
 
 
+//Without using set data structure        SC : O(1)
+
+class Solution {
+public:
+
+    void helper(vector<int>& nums , int n, vector<vector<int>> &ans, vector<int> pair, int ind){
+        
+        ans.push_back(pair);
+        for(int j=ind; j<n; j++){
+
+            if(j != ind  &&  nums[j]==nums[j-1]) continue;
+
+            pair.push_back(nums[j]);
+            helper(nums, n, ans, pair, j+1); 
+            pair.pop_back();
+        }
+        return;
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> pair;
+        int ind=0;
+        sort(nums.begin(), nums.end());
+        helper(nums , n , ans, pair, ind);
+        return ans;
+    }
+};
+
+
+
+---------------------------------------------------------------------------------------------------------------------
+    //using set data structure  SC : O(N)
 
 class Solution {
 public:
