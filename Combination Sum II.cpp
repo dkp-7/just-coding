8dce2758c87845b0e02b92eded8329ed6a86f5b4
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-22 12:32:15 +0530

    Update Combination Sum II.cpp

--- Combination Sum II.cpp
+++ Combination Sum II.cpp
@@ -1,7 +1,39 @@
 // Leetcode :  40. Combination Sum II
 
+//Optimal Approach with less time complexity & no extra space complexity
+
+class Solution {
+public:
+    void helper(vector<int>& candidates, int target, int n , int ind , vector<int> pair , vector<vector<int>> &ans){
+        if(target == 0){
+            ans.push_back(pair);
+            return;
+        }
+        for(int i = ind; i < n ; i++){
+
+            if(i > ind   &&  candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+
+            pair.push_back(candidates[i]);
+            helper(candidates , target-candidates[i] , n , i+1 , pair , ans);    //here always give ind=i+1 and not ind+1, if we give it as ind+1, then it will repeat the loop for same value, which will result in wrong answer
+            pair.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> pair;
+        vector<vector<int>> ans;
+        sort(candidates.begin() , candidates.end());
+        int n = candidates.size();
+        int ind = 0;
+        helper(candidates , target , n , ind , pair, ans);
+        return ans;
+    }
+};
 
 
+---------------------------------------------------------------------------------------------------------------------------------------
+    
 
 //Brute Force Approach , its Time complexity is more & it will be using extra space for hashset
 
