553f665dc2602d1f552c4c7af20d77b3191212a2
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-23 15:59:14 +0530

    Update Permutation Sequence.cpp

--- Permutation Sequence.cpp
+++ Permutation Sequence.cpp
@@ -3,6 +3,50 @@
 
 
 
+// optimised approach
+
+class Solution {
+public:
+    int factorial(int m){
+        if(m == 0) return 1;
+        int f = 1;
+        for(int i=1; i<=m; i++){
+            f = f*i;
+        }
+        return f;
+    }
+
+    void helper(vector<int> nums , int k, int size , string &ans){
+
+        if(size == 0){
+            return;
+        }
+        int ind = k/factorial(size-1);
+        ans += to_string(nums[ind]);
+        nums.erase(nums.begin() + ind);
+        k = k % factorial(size-1);
+
+        helper(nums , k , size-1 , ans);
+    }
+
+    string getPermutation(int n, int k) {
+        
+        vector<int> nums(n,0);
+        for(int i=0; i<n; i++){
+            nums[i] = i+1;
+        }
+        string ans;
+        int size = n;
+        k = k-1;
+        helper(nums , k , size , ans);
+        return ans;
+    }
+};
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------
+
 //Brute Force Approach (Time Limit is Exceeding)
 
 class Solution {
