2a9d589bdc40bff08454cada4ed306e82a1d46de
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-24 20:02:07 +0530

    Create M-Coloring Problem.cpp

---
+++ M-Coloring Problem.cpp
@@ -0,0 +1,45 @@
+// Question Links:
+// Coding Ninja : https://www.codingninjas.com/codestudio/problems/981273?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+// GFG : https://practice.geeksforgeeks.org/problems/m-coloring-problem-1587115620/1
+
+
+#include <bits/stdc++.h> 
+
+bool isPossible(vector<vector<int>> &mat, int v, int clr, vector<int> colour){
+    
+    for(int j=0; j<mat.size(); j++){
+        if(mat[v][j] == 1){
+            if(colour[j] == clr)  return false; 
+        }
+    }
+    return true;
+}
+
+bool helper(vector<vector<int>> &mat, int m, int n, int v, vector<int> colour, int clr){
+    
+    if(v == n) return true;
+    
+    for(int clr=1; clr<=m; clr++){
+        if(isPossible(mat , v , clr , colour)){
+            colour[v] = clr;
+            if(helper(mat, m, n, v+1, colour, clr) == true){
+                return true;
+            }
+            colour[v] = 0;
+        }
+    }
+    return false;
+}
+
+string graphColoring(vector<vector<int>> &mat, int m) {
+
+    int n = mat.size();
+    int v = 0;
+    vector<int> colour(n , 0);
+    int clr  = 0;
+    bool ans = helper(mat , m , n , v , colour , clr);
+    
+    if(ans == true) return "YES";
+    return "NO";
+    
+}
