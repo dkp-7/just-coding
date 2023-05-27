682b89d3878078abb21aafd52fe1b673565396f2
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-27 20:55:07 +0530

    Update Word Break.cpp

--- Word Break.cpp
+++ Word Break.cpp
@@ -1,5 +1,50 @@
 // Word Break Problem
 
+// Question Link (Coding Ninja) : https://www.codingninjas.com/codestudio/problems/983635?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=1
+
+
+
+//this code will not be adding any extra space at the end of the sentence
+
+#include <bits/stdc++.h> 
+
+void helper(string s, vector<string> dictionary, int n, string sentence, vector<string> &ans){
+
+    for(int i=1; i<=n; i++){
+        string ss = s.substr(0 , i);
+        bool flag = false;
+        for(int j=0; j<dictionary.size(); j++){
+            if(dictionary[j] == ss){
+                flag = true;
+                
+            }
+        }
+
+        if(flag == true){
+            if(i == n){
+                sentence = sentence + ss;
+                ans.push_back(sentence);
+                return;
+            }
+            helper(s.substr(i,n-i) , dictionary, n-i, sentence+ss+" ", ans);
+        }
+    }
+}
+
+vector<string> wordBreak(string &s, vector<string> &dictionary)
+{
+    int n = s.size();
+    string sentence = "";
+    vector<string> ans;
+    helper(s, dictionary, n, "", ans);
+    return ans;
+
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------//
+
+// 1 testcase is failing here & at the end of a sentence this code will add an extra space
 
 #include <iostream>
 #include <bits/stdc++.h>
