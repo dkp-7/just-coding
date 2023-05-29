7c64393114830d60d4741f19256f8cf82c4f025e
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-29 17:29:50 +0530

    Update Nth root of a Number.cpp

--- Nth root of a Number.cpp
+++ Nth root of a Number.cpp
@@ -42,3 +42,32 @@ int NthRoot(int n, int m) {
   return -1;
   
 }
+
+
+----------------------------------------------------------------------------------------
+  
+  
+int NthRoot(int n, int m) {
+    
+  int low = 1;
+  int high = m;
+
+  while(low <= high)
+  {
+    int mid = (low + high)/2;
+    
+    double prod = 1.0;
+    for(int i=0; i<n; i++){
+        prod = prod*mid;
+    }
+    
+    if(prod == m)
+      return mid;
+    else if(prod > m)
+      high = mid - 1;
+    else
+      low = mid + 1;
+  }
+
+  return -1; 
+}
