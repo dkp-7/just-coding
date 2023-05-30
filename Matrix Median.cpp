5343ab482db9859a5f4172a5d4e6dada0f6c9342
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-30 20:24:40 +0530

    Update Matrix Median.cpp

--- Matrix Median.cpp
+++ Matrix Median.cpp
@@ -2,6 +2,49 @@
 // Question Link (Coding Ninjas) : https://www.codingninjas.com/codestudio/problems/873378?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
 
 
+// Optimaal Approach using Binary Search
+
+int lessThanOrEqual(vector<int> mat , int limit){
+    int l = 0;
+    int h = mat.size()-1;                //here -1 at the end is very important to pass all given the test case
+    
+    while(l <= h){
+        int md = (l+h)/2;
+        if(mat[md] <= limit){
+            l = md+1;
+        }
+        else h=md-1;
+    }
+    return l;
+}
+
+
+int getMedian(vector<vector<int>> &matrix)
+{
+    int low = 1;
+    int high = 1e9;
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    while(low <= high){
+        int mid = (low+high)/2;
+        int cnt = 0;
+        for(int i=0; i<m; i++){
+            cnt += lessThanOrEqual(matrix[i] , mid);
+        }
+        if(cnt <= (m*n)/2){
+            low = mid+1;
+        }
+        else high=mid-1;
+        
+    }
+    return low;
+}
+
+
+
+---------------------------------------------------------------------------------------------------------------------
+
 //  Naive Approach using extra space
 
 #include <bits/stdc++.h>
