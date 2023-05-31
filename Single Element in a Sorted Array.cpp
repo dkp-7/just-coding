5e7a8517af0278fb84c9adc75e464000e344967f
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-31 17:32:57 +0530

    Update Single Element in a Sorted Array.cpp

--- Single Element in a Sorted Array.cpp
+++ Single Element in a Sorted Array.cpp
@@ -1,5 +1,53 @@
 // Leetcode :   540. Single Element in a Sorted Array
 
+//Optimal method using (Binary Search + XOR)
+
+---------------------------------------------------------------------------
+
+//Optimal method using Binary Search
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        int n = nums.size();
+        int low = 0;
+        int high = n-2;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            //even index, assuming it to be 1st element in duplicate, check left half
+            if(mid%2 == 0){    
+                if(nums[mid] == nums[mid+1]){
+                    //left half confirmed, shrink the left half search space
+                    low = mid+1;
+                }
+                else{
+                    // else, it is from right half, shrink the right half
+                    high = mid-1;
+                }
+            }
+
+            // mid is odd, it maybe index in right half then
+            else{
+                if(nums[mid] == nums[mid+1]){
+                    //right half confirmed, shrink the right half search space
+                    high = mid-1;
+                }
+                else{
+                    // else, it is from left half, shrink the left half
+                    low = mid+1;
+                }
+            }
+        }
+        //when low has crossed high, single element is at index 'low'
+        return nums[low];
+    }
+};
+
+
+-------------------------------------------------------------------------------------------------
 
 class Solution {
 public:
