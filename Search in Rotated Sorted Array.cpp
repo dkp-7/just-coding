6114f1d9886575707b60653693e511e0cfe66dd0
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-05-31 19:50:01 +0530

    Create Search in Rotated Sorted Array.cpp

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,39 @@
+// Leetcode :    33. Search in Rotated Sorted Array
+
+
+//Naive Approach would be using Linear Search  &  Optimal Approach would be using Binary Search
+
+//Otimal Approach  (TC : O(log N)      &&     SC : O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int low = 0;
+        int high = nums.size()-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target) return mid;
+
+            else if(nums[low] <= nums[mid]){
+                if(target >= nums[low]  &&  target < nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+
+            else if(nums[mid] <= nums[high]){
+                if(target > nums[mid]  &&  target <= nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+   
+        }
+        return -1;
+    }
+};
