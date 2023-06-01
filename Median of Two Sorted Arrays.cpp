17006e36b66d21ebcac2dd419cee16144fce5743
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-06-01 20:36:36 +0530

    Update Median of Two Sorted Arrays.cpp

--- Median of Two Sorted Arrays.cpp
+++ Median of Two Sorted Arrays.cpp
@@ -1,43 +1,108 @@
 //Leetcode : 4. Median of Two Sorted Arrays
 
 
+//using Optimsed Merge Sort approach to extract mid elements in sorted array (not using array to store all elements)
+// And as soon as we get both mid elements we break the loop
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        int m = nums1.size();
+        int n = nums2.size();
+        int mid = (m+n)/2;
+        int i=0, j=0, cnt = 0;
+        int md1 =0 , md2 = 0;
+        //vector<int> arr;
+        
+        while(i<m  &&  j<n){
+            if(nums1[i] <= nums2[j]){
+                //arr.push_back(nums1[i]);
+                cnt++;
+                if(cnt == mid){md1 = nums1[i];}
+                if(cnt == mid+1){
+                    md2 = nums1[i];
+                    break;
+                }
+                i++;   
+            }
+            else{
+                //arr.push_back(nums2[j]);
+                cnt++;
+                if(cnt == mid){md1 = nums2[j];}
+                if(cnt == mid+1){
+                    md2 = nums2[j];
+                    break;
+                }
+                j++;
+            }
+        }
+        
+        while(i<m){
+            //arr.push_back(nums1[i]);
+            cnt++;
+            if(cnt == mid){md1 = nums1[i];}
+            if(cnt == mid+1){
+                md2 = nums1[i];
+                break;
+            }
+            i++;
+        }
+        while(j < n){
+            //arr.push_back(nums2[j]);
+            cnt++;
+            if(cnt == mid){md1 = nums2[j];}
+            if(cnt == mid+1){
+                md2 = nums2[j];
+                break;
+            }
+            j++;
+ 
+        }
+        double median = 0;
+        if((m+n)%2 == 0){
+            median = (md1+md2) / 2.0;
+            }
+        else{
+            median = (double)md2; 
+        }
+        
+        return median;
+    }
+};
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
 // using merge sort
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        //using merge Sort approach to extract mid elements in sorted array
         int m = nums1.size();
         int n = nums2.size();
         int mid = (m+n)/2;
-        int i=0, j=0, cnt = 0;
-        int md1 =0 , md2 = 0;
+        int i=0, j=0;
         vector<int> arr;
         
         while(i<m  &&  j<n){
             if(nums1[i] <= nums2[j]){
                 arr.push_back(nums1[i]);
-                cnt++;
                 i++;   
             }
             else{
                 arr.push_back(nums2[j]);
-                cnt++;
                 j++;
             }
-        }
-        
+        }  
         while(i<m){
             arr.push_back(nums1[i]);
-            cnt++;
             i++;
         }
         while(j < n){
             arr.push_back(nums2[j]);
-            cnt++;
             j++;
- 
         }
         double median = 0;
         if((m+n)%2 == 0){
@@ -45,8 +110,7 @@ public:
             }
         else{
             median = (double)arr[mid]; 
-        }
-        
+        } 
         return median;
     }
 };
