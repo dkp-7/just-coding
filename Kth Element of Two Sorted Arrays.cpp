28e06fa4287ae7eef825328ecb40558ee80924db
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-06-02 17:34:38 +0530

    Update Kth Element of Two Sorted Arrays.cpp

--- Kth Element of Two Sorted Arrays.cpp
+++ Kth Element of Two Sorted Arrays.cpp
@@ -2,6 +2,57 @@
 //Question Link (Coding Ninjas) : https://www.codingninjas.com/codestudio/problems/1112629?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
 
 
+// Using Merge Sort but no extra space
+
+int ninjaAndLadoos(vector<int> &row1, vector<int> &row2, int m, int n, int k) {
+
+    //vector<int> merge;
+    int ans;
+    int i=0, j=0;
+    int cnt = 0;
+    while(i<m  &&  j<n){
+        if(row1[i] <= row2[j]){
+            cnt++;
+            if(cnt == k){
+                ans = row1[i];
+                break;
+            }
+            i++;
+        }
+        else{
+            cnt++;
+            if(cnt == k){
+                ans = row2[j];
+                break;
+            }
+            j++;
+        }
+    }
+    while(i < m){
+        cnt++;
+        if(cnt == k){
+            ans = row1[i];
+            break;
+        }
+        i++;
+    }
+    while(j < n){
+        cnt++;
+        if(cnt == k){
+            ans = row2[j];
+            break;
+        }
+        j++;
+    }  
+
+    return ans;
+}
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+//Using Merge Sort + Extra Space of O(m+n)
 
 int ninjaAndLadoos(vector<int> &row1, vector<int> &row2, int m, int n, int k) {
     
