5d7c6155c24d9e1170ad9a3ec53d430bb1719743
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-06-06 15:25:13 +0530

    Create K Max Sum Combinations.cpp

---
+++ K Max Sum Combinations.cpp
@@ -0,0 +1,35 @@
+// Question (Coding Ninjas):  K Max Sum Combinations
+//Link :  https://www.codingninjas.com/codestudio/problems/k-max-sum-combinations_975322?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+#include <bits/stdc++.h> 
+
+void check(vector<int> &ans, int sum){
+	int minimum = ans[0];
+	int ind=0;
+	for(int i=1; i<ans.size(); i++){
+		if(minimum > ans[i]){
+			minimum = ans[i];
+			ind = i;
+		}
+	}
+	if(minimum < sum){
+		ans[ind] = sum;
+	}
+}
+
+bool compare(int a , int b){
+	return a > b;
+}
+
+vector<int> kMaxSumCombination(vector<int> &a, vector<int> &b, int n, int k){
+
+	vector<int> ans(k,INT_MIN);
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			int sum = a[i]+b[j];
+			check(ans, sum);
+		}
+	}
+	sort(ans.begin() , ans.end() , compare);
+	return ans;
+}
