3f8e8e58030de8c56013608a3b47d519cedff8df
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-06-09 18:12:43 +0530

    Update Find the Index of the First Occurrence in a String.cpp

--- Find the Index of the First Occurrence in a String.cpp
+++ Find the Index of the First Occurrence in a String.cpp
@@ -1,6 +1,32 @@
 //  Leetcode :   28. Find the Index of the First Occurrence in a String
 
 
+// More optimal using 2 pointer
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        int n = haystack.length();
+        int len = needle.length();
+        
+        if(haystack == needle) return 0;
+
+        for(int i=0; i<n-len+1; i++){
+            int ls=i;  int lt=0;
+            int rs=i+len-1;    int rt=lt+len-1;
+            while(lt<=len  &&  rt>=0 && haystack[ls]==needle[lt]  &&  haystack[rs]==needle[rt]){
+                ls++;  lt++;
+                rs--;  rt--;
+            }
+            if(ls>rs) return i;
+        }
+        return -1;
+    }
+};
+
+--------------------------------------------------------------------------------
+    
 class Solution {
 public:
     int strStr(string haystack, string needle) {
