48e615ff176d43db7843024b0b484f1d9ad32172
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-06-09 13:24:49 +0530

    Update String to Integer (atoi).cpp

--- String to Integer (atoi).cpp
+++ String to Integer (atoi).cpp
@@ -1,6 +1,40 @@
 // Leetcode :   8. String to Integer (atoi)
 
 
+class Solution {
+public:
+    int myAtoi(string s) {
+        
+        int i = 0;
+        int sign = 1;
+        int count = 0;// count optional sign number
+        int res = 0;
+        if (s.empty())return 0;
+        
+        //Discard whitespaces in the beginning
+        while(i < s.size() && s[i] == ' ')
+            i++;
+        
+        //Check if optional sign if it exists
+        while(i < s.size() && (s[i] == '+' || s[i] == '-')){
+            count++;
+            sign = (s[i++] == '-')? -1 : 1;
+        }
+        if(count>1) count = 0;
+        else count = 1;
+        
+        //Build the result and check for overflow/underflow condition
+        while(i < s.size() && s[i]>='0' && s[i]<='9')                     //   or     while(i < s.size() && (isdigit(s[i]) == true))
+        {
+            if(res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)){
+                return(sign == 1)? INT_MAX : INT_MIN;
+            } 
+            res = res * 10 + (s[i++] - '0');
+        }
+      return res * sign*count;
+    }
+};
+
 
 ----------------------------------------------------------------------------------
 
