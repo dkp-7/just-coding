b9f9cc06957adb2744820e2eced29193adbba8c3
dkp-7 <120702996+dkp-7@users.noreply.github.com>
2023-06-10 16:39:49 +0530

    Update Compare Version Numbers.cpp

--- Compare Version Numbers.cpp
+++ Compare Version Numbers.cpp
@@ -4,6 +4,37 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
+
+        int n1 = version1.length();
+        int n2 = version2.length();
+        int sum1=0, sum2=0;
+        int i=0, j=0;
+
+        while((i < n1)  ||  (j < n2)){
+            sum1=0;
+            sum2=0;
+            while(i<n1  &&  version1[i] != '.'){
+                sum1 = sum1*10 + (version1[i]-'0');
+                i++;
+            }
+            while(j<n2  &&  version2[j] != '.'){
+                sum2 = sum2*10 + (version2[j]-'0');
+                j++;
+            }
+            if(sum1 > sum2) return 1;
+            else if(sum2 > sum1) return -1;
+            i++;  j++;
+        }
+        return 0;
+    }
+};
+
+------------------------------------------------------------------------
+
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
         
         if(version1 == version2) return 0;
         int n1 = version1.length();
